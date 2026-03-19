faa1bbbd067771873744bd4b4c36796f837b221d
Afzal1001 <mdafz.spc@gmail.com>
2026-03-19 19:16:02 +0530

    Create init_database.sql to set up DataWarehouse
    This script creates a new database named 'DataWarehouse' along with three schemas: bronze, silver, and gold. It also ensures that any existing database with the same name is dropped before creating the new one.

---
+++ scripts/init_database.sql
@@ -0,0 +1,28 @@
+/* 
+this scripts creates a new database datawarehouse 
+*/
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse' )
+BEGIN 
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+  DROP DATABASE DataWarehouse;
+END
+
+  
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+--CREATE SCHEMAS   
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
